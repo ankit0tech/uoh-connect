d90b85509f448e7262377c8d03d9a6638717552b
Ankit Punia <ankitsingh.as495@gmail.com>
2021-03-03 21:29:38 +0530

    removed STATICFILES_STORAGE AWS variable

--- connectus/settings.py
+++ connectus/settings.py
@@ -155,7 +155,6 @@ django_heroku.settings(locals())
 
 # AWS S3 bucket setup
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = 'ap-south-1'
 AWS_S3_SIGNATURE_VERSION  = 's3v4'
